9bd34885eb85bda4db06a2d81b92bdb229f3150d
sanjai kumar n <nsksanjai@gmail.com>
2022-03-01 21:20:07 +0530

    fianl commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 function Footer() {
-    const Year = new Date().getFullYear();
-    return(
-        <footer>
-            <p>copyright © {Year}</p>
-        </footer>
-    );
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
+
 export default Footer;
